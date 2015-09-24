2c2e845891ff66e2534d21b07dc7839fc51d6f55
misha <interlekt@gmail.com>
2015-09-24 22:22:20 +0300

    testing angular views

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,49 @@
-// Parcmate client side JS
+var app = angular.module('parcMate', [
+  'ngRoute',
+  'mobile-angular-ui',
+  'mobile-angular-ui.gestures'
+]);
+app.run(function($transform) {
+  window.$transform = $transform;
+});
+
+app.config(function($routeProvider) {
+  $routeProvider.when('/', {templateUrl: 'index.html', reloadOnSearch: false});
+});
+
+app.controller('parcMateController', function ($rootScope, $scope) {
+  $scope.swiped = function(direction) {
+    alert('Swiped ' + direction);
+  };
+
+  // User agent displayed in home page
+  $scope.userAgent = navigator.userAgent;
+
+  // Needed for the loading screen
+  $rootScope.$on('$routeChangeStart', function(){
+    $rootScope.loading = true;
+  });
+
+  $rootScope.$on('$routeChangeSuccess', function(){
+    $rootScope.loading = false;
+  });
+
+  // Fake text i used here and there.
+  $scope.lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel explicabo, aliquid eaque soluta nihil eligendi adipisci error, illum corrupti nam fuga omnis quod quaerat mollitia expedita impedit dolores ipsam. Obcaecati.';
+
+  //
+  // 'Scroll' screen
+  //
+  var scrollItems = [];
+
+  for (var i=1; i<=100; i++) {
+    scrollItems.push('Item ' + i);
+  }
+
+  $scope.scrollItems = scrollItems;
+
+  $scope.bottomReached = function() {
+    /* global alert: false; */
+    alert('Congrats you scrolled to the end of the list!');
+  };
+});
