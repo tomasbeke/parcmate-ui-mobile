479f7486dfbbf84b86a8a744386516b19ee4452c
misha <interlekt@gmail.com>
2015-09-25 01:03:20 +0300

    add test mobile templates

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,7 +8,19 @@ app.run(function($transform) {
 });
 
 app.config(function($routeProvider) {
-  $routeProvider.when('/', {templateUrl: 'index.html', reloadOnSearch: false});
+  $routeProvider.when('/', {templateUrl: '../../../home.html', reloadOnSearch: false});
+  // $routeProvider.when('/scroll',        {templateUrl: 'scroll.html', reloadOnSearch: false});
+  // $routeProvider.when('/toggle',        {templateUrl: 'toggle.html', reloadOnSearch: false});
+  $routeProvider.when('/tabs',          {templateUrl: '../../../tabs.html', reloadOnSearch: false});
+  // $routeProvider.when('/accordion',     {templateUrl: 'accordion.html', reloadOnSearch: false});
+  // $routeProvider.when('/overlay',       {templateUrl: 'overlay.html', reloadOnSearch: false});
+  // $routeProvider.when('/forms',         {templateUrl: 'forms.html', reloadOnSearch: false});
+  // $routeProvider.when('/dropdown',      {templateUrl: 'dropdown.html', reloadOnSearch: false});
+  // $routeProvider.when('/touch',         {templateUrl: 'touch.html', reloadOnSearch: false});
+  // $routeProvider.when('/swipe',         {templateUrl: 'swipe.html', reloadOnSearch: false});
+  // $routeProvider.when('/drag',          {templateUrl: 'drag.html', reloadOnSearch: false});
+  // $routeProvider.when('/drag2',         {templateUrl: 'drag2.html', reloadOnSearch: false});
+  // $routeProvider.when('/carousel',      {templateUrl: 'carousel.html', reloadOnSearch: false});
 });
 
 app.controller('parcMateController', function ($rootScope, $scope) {
