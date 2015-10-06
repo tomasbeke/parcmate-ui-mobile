3ae0e205f3d7b1f23e3bada1fe68b0e0e85ef353
misha <interlekt@gmail.com>
2015-10-07 01:05:03 +0300

    update map view

---
+++ src/assets/js/maps.js
@@ -0,0 +1,21 @@
+var map;
+
+function initMap () {
+  man = new google.maps.Map(document.getElementById('map'), {
+    center : {lat: -34.397, lng: 150.644},
+    zoom : 8
+  });
+}
+
+function detectBrowser() {
+  var useragent = navigator.userAgent;
+  var mapdiv = document.getElementById("map");
+
+  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '100%';
+  } else {
+    mapdiv.style.width = '600px';
+    mapdiv.style.height = '800px';
+  }
+}
