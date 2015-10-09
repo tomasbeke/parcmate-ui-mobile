6fd31897463cf83bce6419987027fc9fa669ccaf
misha <interlekt@gmail.com>
2015-10-09 18:26:20 +0300

    update maps

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -46,32 +46,95 @@ app.controller('parcMateController', function ($rootScope, $scope) {
   $rootScope.$on('$routeChangeSuccess', function(e, toState){
     var view = toState.$$route.originalPath.replace(/\//g, '');
     $rootScope.state = view;
-    // if (view === 'find-garage') {
-    //   loadScripts('assets/js/maps.js');
-    //   loadScripts('https://maps.googleapis.com/maps/api/js?key=AIzaSyAOqMgt-ZS0td_lWiQYD6cSMQ5V9ID6MRI&callback=initMap');
-    // }
+    if (view === 'find-garage') {
+      loadScripts('assets/js/maps.js');
+      loadScripts('https://maps.googleapis.com/maps/api/js?key=AIzaSyAOqMgt-ZS0td_lWiQYD6cSMQ5V9ID6MRI&callback=initMap');
+    }
     $rootScope.loading = false;
-    scrollableOptions();
-    findItemInScroll();
+    initHelpers();
   });
 });
 
 // Mock/prototype behavior for control scroll.
 
+function initHelpers() {
+  scrollableOptions();
+  findItemInScroll();
+  registerScroll();
+}
+
 function scrollableOptions () {
   var itemsWidth = 0;
   $('.scrollable-options .option').map(function (i) {
-    j = i;
     itemsWidth = itemsWidth + $(this).outerWidth(true);
   });
   $('.scrollable-options').css('width', (itemsWidth+15));
 }
 
 function findItemInScroll () {
-  var html = '<div class="col-xs-1 toggle"><a href="#" class="toggle"><i class="fa fa-chevron-left"></i></a></div>';
+  var html = '<div class="col-xs-1 toggle"><a href="#"><i class="fa fa-chevron-left"></i></a></div>';
   $('.scrollable-options .option').map(function (i) {
     if (i === 2) {
       $(this).after(html)
+    }
+  });
+}
+
+function registerScroll () {
+  $('.options').on('scroll', function (a, b) {
+    var containerLPos = $('.options').position().left,
+        containerRPos = containerLPos+$('.options').outerWidth(),
+        toggleLPos = $('.toggle').position().left;
+
+    var item3 = $('.options').find('.option').eq(2);
+
+    var item = $('.options').find('.option').length,
+        itemW = containerRPos/item,
+        thirdItem = itemW*5,
+        containerRelPos = containerRPos-itemW
+        resetPos = containerRelPos + item3.position().left
+
+    //console.log(item3.position().left, containerLPos)
+
+    if (toggleLPos <= containerLPos) {
+
+      $('.toggle')
+        .addClass('fixed')
+        .find('.fa')
+        .removeClass('fa-chevron-left')
+        .addClass('fa-chevron-right');
+
+      containerLPos = thirdItem;
+
+      console.log(
+        'item width: ' + itemW,
+        '3rd item: '+ thirdItem,
+        'container left pos: ' + containerLPos,
+        'container right pos ' + containerRPos,
+        'toggle pos: ' + toggleLPos,
+        'container relative pos: ' + containerRelPos
+      )
+      console.log(containerLPos, resetPos, ' IF')
+
+    }
+
+    if (resetPos >= containerLPos) {
+
+      $('.toggle')
+        .removeClass('fixed')
+        .find('.fa')
+        .removeClass('fa-chevron-right')
+        .addClass('fa-chevron-left');
+
+      console.log(
+        'item width: '+containerRPos/item,
+        '3rd item: '+ (containerRPos/item)*3,
+        'container left pos: '+containerLPos,
+        'container right pos '+containerRPos,
+        'toggle pos: ' + toggleLPos,
+        'container relative pos: '+ (containerRPos-toggleLPos)
+      )
+      console.log(item3.position().left, containerLPos, ' ELSE')
     }
   });
 }
--- src/assets/js/maps.js
+++ src/assets/js/maps.js
@@ -4,12 +4,18 @@ function initMap () {
   var mapOptions = {
     zoom : 15,
     center : {lat: 40.748817, lng: -73.985428},
-    mapTypeControl : false,
+    // Street View
     streetViewControl : true,
-    styleOptions : {
-      style : google.maps
-    },
-    rotateControl : true
+    streetViewControlOptions : {},
+    // Zoom Control
+    zoomControl: true,
+    zoomControlOptions : {},
+    // Map Type Control
+    mapTypeControl: true,
+    mapTypeControlOptions : {},
+    // scaleControl: true,
+    // streetViewControl: true,
+    // rotateControl : true
     //disableDefaultUI : true
   }
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
