c3cc984a87531fb62ba3a9431235efb1fb5c4032
misha <interlekt@gmail.com>
2015-10-26 23:00:07 +0300

    update accordion :225

--- src/assets/js/accordion.js
+++ src/assets/js/accordion.js
@@ -2,13 +2,25 @@ var accordion = {
   init: function() {
     this.cacheDom();
     this.bindEvents();
-    console.log(this)
   },
   cacheDom : function () {
-    this.$elTrigger = $('.js-accordion-trigger');
+    this.$elTrigger = $('.js-trigger');
   },
   bindEvents : function () {
     this.$elTrigger.on('click', this.elExpand);
   },
-  elExpand : function (e) {}
+  elExpand : function (e) {
+    e.preventDefault();
+    var $el = $(this),
+        $elContent = $el.parent().next('.section-body'),
+        $elParent = $el.parents('.info-section'),
+        $elSiblings = $elParent.siblings();
+
+    if (!$elParent.hasClass('is-expanded')) {
+      $elParent.addClass('is-expanded');
+      $elSiblings.removeClass('is-expanded');
+    } else {
+      $elParent.removeClass('is-expanded');
+    }
+  }
 };
